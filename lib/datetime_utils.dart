687d69cf83513f653ac1b18b3b8275c9f0342f52
Gary <gary@wonggar.net>
2023-10-07 17:02:02 +0800

    date time utils for testing

--- lib/datetime_utils.dart
+++ lib/datetime_utils.dart
@@ -8,4 +8,9 @@ class DateTimeUtils {
     return format_yMdHm.format(dateTime);
   }
 
+  // Return mockedDateTime if not null and mocked = true, otherwise DateTime.now()
+  static DateTime getCurrentDateTime({bool mocked = false, DateTime? mockedDateTime}) {
+    return mocked && mockedDateTime != null ? mockedDateTime : DateTime.now();
+  }
+
 }
