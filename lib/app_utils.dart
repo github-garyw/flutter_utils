2b5fe189e1fbd4768371916d3a9f23b0ee5df0f6
Gary <gary@wonggar.net>
2023-11-14 01:53:51 +0800

    Revert "Revert "app mkdir and write to file methods""
    This reverts commit a3ed74c50386f4c654f412115812ba41981a9cb7.

--- lib/app_utils.dart
+++ lib/app_utils.dart
@@ -1,9 +1,44 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:fluttertoast/fluttertoast.dart';
 
 class AppUtils {
+
+  static Future<bool> writeToFile(String dirPath, String fileName, String content) async {
+    await mkdir(dirPath);
+
+    String filePath = '$dirPath/$fileName';
+    // Open the file in write mode (creates the file if it doesn't exist)
+    File file = File(filePath);
+
+    // Write the content to the file
+    file.writeAsString(content).then((_) {
+      print('File written successfully to $dirPath/$fileName');
+    }).catchError((error) {
+      print('Error writing to file $dirPath/$fileName: $error');
+      return false;
+    });
+
+    return true;
+  }
+
+  static Future<void> mkdir(String dirPath) async {
+    if (await Directory(dirPath).exists()) {
+      return;
+    }
+
+    print('mkdir $dirPath');
+
+    // Create directory if it doesn't exist
+    await Directory(dirPath).create(recursive: true).then((Directory directory) {
+      print('Directory created: ${directory.path}');
+    }).catchError((error) {
+      print('Error creating directory: $error');
+    });
+  }
 
   static List<T> castDynamicList<T>(List<dynamic> dataList) {
     return dataList.map((e) => e as T).toList();
