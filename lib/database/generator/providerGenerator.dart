4341eb8f67661a9c582b1a23495fda8afa8d596e
Gary Desktop <gary@wonggar.net>
2024-04-05 22:23:44 +0800

    added update object to provider

--- lib/database/generator/providerGenerator.dart
+++ lib/database/generator/providerGenerator.dart
@@ -52,10 +52,15 @@ class ProviderGenerator {
 
       classContent += _getPreloadedOwnedData(schema);
       classContent += END_OF_LINE;
-
     }
 
-    classContent += _getUpdateMethod();
+    classContent += _getNotifyMethod();
+    classContent += END_OF_LINE;
+
+    classContent += _getReplace(schema);
+    classContent += END_OF_LINE;
+
+    classContent += _getUpdateMethod(schema);
     classContent += END_OF_LINE;
 
     classContent += '}$END_OF_LINE';
@@ -70,18 +75,18 @@ class ProviderGenerator {
     final className = schema.metaData[CLASS_NAME];
     var ret = '';
     ret +=
-    '${TAB}static ${className}? queryPreloadedDataById(int id) {$END_OF_LINE';
-    ret += '${TAB}${TAB}return _allData?.where((element) => element.id == id).firstOrNull;$END_OF_LINE';
+        '${TAB}static ${className}? queryPreloadedDataById(int id) {$END_OF_LINE';
+    ret +=
+        '${TAB}${TAB}return _allData?.where((element) => element.id == id).firstOrNull;$END_OF_LINE';
     ret += '${TAB}}$END_OF_LINE';
     return ret;
   }
 
   static String _getReloadAll(Schema schema) {
     var ret = '';
-    ret +=
-    '${TAB}Future<void> reloadAll() async {$END_OF_LINE';
+    ret += '${TAB}Future<void> reloadAll() async {$END_OF_LINE';
     ret += '${TAB}${TAB}await _downloadAll();$END_OF_LINE';
-    ret += '${TAB}${TAB}update();$END_OF_LINE';
+    ret += '${TAB}${TAB}notify();$END_OF_LINE';
     ret += '${TAB}}$END_OF_LINE';
     return ret;
   }
@@ -89,10 +94,9 @@ class ProviderGenerator {
   static String _getReloadOwned(Schema schema) {
     final className = schema.metaData[CLASS_NAME];
     var ret = '';
-    ret +=
-    '${TAB}Future<void> reloadOwned() async {$END_OF_LINE';
+    ret += '${TAB}Future<void> reloadOwned() async {$END_OF_LINE';
     ret += '${TAB}${TAB}await _downloadOwned();$END_OF_LINE';
-    ret += '${TAB}${TAB}update();$END_OF_LINE';
+    ret += '${TAB}${TAB}notify();$END_OF_LINE';
     ret += '${TAB}}$END_OF_LINE';
     return ret;
   }
@@ -101,7 +105,7 @@ class ProviderGenerator {
     final className = schema.metaData[CLASS_NAME];
     var ret = '';
     ret +=
-    '${TAB}Future<Triple<bool, List<$className>?, String>> getAll$className() async {$END_OF_LINE';
+        '${TAB}Future<Triple<bool, List<$className>?, String>> getAll$className() async {$END_OF_LINE';
     ret += '${TAB}${TAB}return await sGetAll${className}();$END_OF_LINE';
     ret += '${TAB}}$END_OF_LINE';
     return ret;
@@ -111,15 +115,33 @@ class ProviderGenerator {
     final className = schema.metaData[CLASS_NAME];
     var ret = '';
     ret +=
-    '${TAB}Future<Triple<bool, List<$className>?, String>> getUser$className() async {$END_OF_LINE';
+        '${TAB}Future<Triple<bool, List<$className>?, String>> getUser$className() async {$END_OF_LINE';
     ret += '${TAB}${TAB}return await sGetUser${className}();$END_OF_LINE';
     ret += '${TAB}}$END_OF_LINE';
     return ret;
   }
 
-  static String _getUpdateMethod() {
+  static String _getUpdateMethod(Schema schema) {
+    final className = schema.metaData[CLASS_NAME];
     var ret = '';
-    ret += '${TAB}void update(){$END_OF_LINE';
+    ret +=
+        '${TAB}Future<Triple<bool, $className?, String>> update($className obj) async {$END_OF_LINE';
+    ret += '${TAB}${TAB}final res = await $className.update(obj);$END_OF_LINE';
+    ret += '''${TAB}${TAB}if (res.first) {
+			final newObj = res.middle!;
+			if (_IS_USER_TABLE) {
+				_replace(_ownedData, newObj);
+			}
+			_replace(_allData, newObj);
+		}''';
+    ret += '${TAB}${TAB}return res;$END_OF_LINE';
+    ret += '${TAB}}$END_OF_LINE';
+    return ret;
+  }
+
+  static String _getNotifyMethod() {
+    var ret = '';
+    ret += '${TAB}void notify(){$END_OF_LINE';
     ret += '${TAB}${TAB}notifyListeners();$END_OF_LINE';
     ret += '${TAB}}$END_OF_LINE';
     return ret;
@@ -129,7 +151,7 @@ class ProviderGenerator {
     final className = schema.metaData[CLASS_NAME];
     var ret = '';
     ret +=
-    '${TAB}static Future<Triple<bool, List<$className>?, String>> sGetAll$className() async {$END_OF_LINE';
+        '${TAB}static Future<Triple<bool, List<$className>?, String>> sGetAll$className() async {$END_OF_LINE';
     ret += '${TAB}${TAB}if(_allData != null){$END_OF_LINE';
     ret += '${TAB}${TAB}${TAB}return Triple(true, _allData, "");$END_OF_LINE';
     ret += '${TAB}${TAB}}$END_OF_LINE';
@@ -143,7 +165,7 @@ class ProviderGenerator {
     final className = schema.metaData[CLASS_NAME];
     var ret = '';
     ret +=
-    '${TAB}static Future<Triple<bool, List<$className>?, String>> sGetUser$className() async {$END_OF_LINE';
+        '${TAB}static Future<Triple<bool, List<$className>?, String>> sGetUser$className() async {$END_OF_LINE';
     ret += '${TAB}${TAB}if(_ownedData != null){$END_OF_LINE';
     ret += '${TAB}${TAB}${TAB}return Triple(true, _ownedData, "");$END_OF_LINE';
     ret += '${TAB}${TAB}}$END_OF_LINE';
@@ -157,7 +179,7 @@ class ProviderGenerator {
     final className = schema.metaData[CLASS_NAME];
     var ret = '';
     ret +=
-    '${TAB}static Future<Triple<bool, List<$className>?, String>> _downloadAll() async {$END_OF_LINE';
+        '${TAB}static Future<Triple<bool, List<$className>?, String>> _downloadAll() async {$END_OF_LINE';
     ret += END_OF_LINE;
     ret += '${TAB}${TAB}final supabase = Supabase.instance.client;$END_OF_LINE';
     ret += END_OF_LINE;
@@ -167,16 +189,16 @@ class ProviderGenerator {
     ret += '${TAB}${TAB}${TAB}${TAB}.select() as List<dynamic>;$END_OF_LINE';
     ret += END_OF_LINE;
     ret +=
-    '${TAB}${TAB}${TAB}final result = data?.map((json) => ${className}.fromJson(json)).toList();$END_OF_LINE';
+        '${TAB}${TAB}${TAB}final result = data?.map((json) => ${className}.fromJson(json)).toList();$END_OF_LINE';
     ret += '${TAB}${TAB}${TAB}_allData = result;$END_OF_LINE';
     ret +=
-    '${TAB}${TAB}${TAB}_allData?.sort((a, b) => a.createdAt!.compareTo(b.createdAt!));$END_OF_LINE';
+        '${TAB}${TAB}${TAB}_allData?.sort((a, b) => a.createdAt!.compareTo(b.createdAt!));$END_OF_LINE';
     ret += '$TAB${TAB}${TAB}return Triple(true, _allData, "");$END_OF_LINE';
     ret += '${TAB}${TAB}} catch (e, s) {$END_OF_LINE';
     ret += '$TAB${TAB}${TAB}print(e);$END_OF_LINE';
     ret += '$TAB${TAB}${TAB}print(s);$END_OF_LINE';
     ret +=
-    '$TAB${TAB}${TAB}return Triple(false, null, e.toString());$END_OF_LINE';
+        '$TAB${TAB}${TAB}return Triple(false, null, e.toString());$END_OF_LINE';
     ret += '${TAB}${TAB}}$END_OF_LINE';
     ret += END_OF_LINE;
     ret += '${TAB}}$END_OF_LINE';
@@ -226,22 +248,28 @@ class ProviderGenerator {
 
   static String _getPreloadedOwnedData(Schema schema) {
     var ret = '';
-    ret += '${TAB}static List<${schema.metaData[CLASS_NAME]}>? getPreloadedUserData() {$END_OF_LINE';
-    ret += '${TAB}${TAB}return _ownedData == null ? null : List.of(_ownedData!);$END_OF_LINE';
+    ret +=
+        '${TAB}static List<${schema.metaData[CLASS_NAME]}>? getPreloadedUserData() {$END_OF_LINE';
+    ret +=
+        '${TAB}${TAB}return _ownedData == null ? null : List.of(_ownedData!);$END_OF_LINE';
     ret += '${TAB}}$END_OF_LINE';
     return ret;
   }
 
   static String _getPreloadedAllData(Schema schema) {
     var ret = '';
-    ret += '${TAB}static List<${schema.metaData[CLASS_NAME]}>? getPreloadedAllData() {$END_OF_LINE';
-    ret += '${TAB}${TAB}return _allData == null ? null : List.of(_allData!);$END_OF_LINE';
+    ret +=
+        '${TAB}static List<${schema.metaData[CLASS_NAME]}>? getPreloadedAllData() {$END_OF_LINE';
+    ret +=
+        '${TAB}${TAB}return _allData == null ? null : List.of(_allData!);$END_OF_LINE';
     ret += '${TAB}}$END_OF_LINE';
     return ret;
   }
 
   static String _getMembers(Schema schema) {
     var ret = '';
+    ret +=
+        '${TAB}static const bool _IS_USER_TABLE = ${schema.metaData[IS_USER_TABLE]};$END_OF_LINE${END_OF_LINE}';
     ret +=
         '${TAB}static List<${schema.metaData[CLASS_NAME]}>? _allData;$END_OF_LINE';
 
@@ -261,6 +289,23 @@ class ProviderGenerator {
     ret += "import 'package:flutter_utils/triple.dart';$END_OF_LINE";
     ret += "import '${schema.metaData[DART_FILE_NAME]}';$END_OF_LINE";
 
+    return ret;
+  }
+
+  static String _getReplace(Schema schema) {
+    final className = schema.metaData[CLASS_NAME];
+    var ret =
+        '''${TAB}static void _replace(List<$className>? list, $className newObj) {
+		if (list == null) {
+			return;
+		}
+		
+		final index = list.indexWhere((element) => element.id == newObj.id);
+		if (index != -1) {
+			list[index] = newObj;
+		}
+	}
+	''';
     return ret;
   }
 }
